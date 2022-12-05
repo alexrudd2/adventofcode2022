b88f884e5229cb20c8079e4fc82218887008f018
Alex Ruddick <a.ruddick@numat-tech.com>
2022-12-04 20:51:33 -0600

    Day 2, part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+fn main() {
+
+    let input = File::open("../input.txt").expect("Could not read input");    
+    let reader = BufReader::new(input);
+    let lines: Vec<_> = reader.lines().collect();
+
+    let mut total_score_part_1: i32 = 0;
+    for line in lines {
+        let round_score: i32 = match line.expect("Could not parse line").trim() {
+            "A X" => 3 + 1,  //rock = rock
+            "B X" => 0 + 1,  //paper > rock
+            "C X" => 6 + 1,  //scissors > rock
+            "A Y" => 6 + 2,  //rock < paper
+            "B Y" => 3 + 2,  //paper = paper
+            "C Y" => 0 + 2,  //scissors > paper
+            "A Z" => 0 + 3,  //rock > scissors
+            "B Z" => 6 + 3,  //paper < scissors
+            "C Z" => 3 + 3,  //scissors = scissors
+            _ => 0,
+        };
+        total_score_part_1 += round_score;
+    };
+    println!("Your total score for part 1 is {total_score_part_1}.");
+}
