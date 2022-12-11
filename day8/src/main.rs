6fceca2c60a2ceef60efd21df715acc56c07a70d
Alex Ruddick <a.ruddick@numat-tech.com>
2022-12-11 15:56:53 -0600

    Day 8, part 2: too lazy to functionalize this

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     let cols: usize = <Vec<Result<String, std::io::Error>> as AsRef<Vec<_>>>::as_ref(&lines)[0].as_ref().unwrap().len();
     let mut forest = Array2::<i8>::default((rows, cols));
     let mut visibility = Array2::<i8>::zeros((rows, cols));
+    let mut scenic = Array2::<i32>::zeros((rows, cols));
 
     let mut row: usize = 0;
     for l in lines {
@@ -25,11 +26,12 @@ fn main() {
         row += 1;
     }
 
-    let mut total = 0;
+    let mut total_trees = 0;
+    let mut max_scenic_score: i32 = 0;
     for row in 0..rows {
         for col in 0..cols {
             if row == 0 || col == 0 || row == rows - 1 || col == cols - 1{
-                visibility[[row, col]] = 1; total += 1;
+                visibility[[row, col]] = 1; total_trees += 1;
             } else {
                 let height = forest[[row, col]];
                 let top_slice = forest.slice(s![0..row, col]);
@@ -41,17 +43,43 @@ fn main() {
                 let tallest_left = left_slice.iter().max().expect("Empty array");
                 let tallest_right = right_slice.iter().max().expect("Empty array");
                 if height > *tallest_top || height > *tallest_bot || height > *tallest_right || height > *tallest_left {
-                    visibility[[row, col]] = height as i8; total += 1;
+                    visibility[[row, col]] = height as i8; total_trees += 1;
                 }
+                let mut scenic_score: i32;
+                let mut trees_visible = 0;
+                for other_tree in top_slice.iter().rev() {
+                    trees_visible += 1;
+                    if height <= *other_tree { break }
+                }
+                scenic_score = trees_visible;
+
+                trees_visible = 0;
+                for other_tree in bot_slice.iter() {
+                    trees_visible += 1;
+                    if height <= *other_tree { break }
+                }
+                scenic_score = scenic_score * trees_visible;
+
+                trees_visible = 0;
+                for other_tree in left_slice.iter().rev() {
+                    trees_visible += 1;
+                    if height <= *other_tree { break }
+                }
+                scenic_score = scenic_score * trees_visible;
+
+                trees_visible = 0;
+                for other_tree in right_slice.iter() {
+                    trees_visible += 1;
+                    if height <= *other_tree { break }
+                }
+                scenic[[row, col]] = scenic_score;
+                scenic_score = scenic_score * trees_visible;
+                if scenic_score > max_scenic_score { max_scenic_score = scenic_score;}
             }
         }
     }
-    println!("There are {total} trees visible."); //1820
+    println!("There are {total_trees} trees visible."); //1820
+    println!("The most scenic has a score of {max_scenic_score}."); //385112
     println!("Perhaps if asked nicely the elves allow guests in their tree house?")
-    // for row in 0..rows {
-    //     for col in 0..cols {
-    //         print!("{}", visibility[[row, col]]);
-    //     }
-    //     print!("\n");
-    // }
+
 }
