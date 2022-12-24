79a69148852a8a309ce66196236a783a95dc3d57
Alex Ruddick <a.ruddick@numat-tech.com>
2022-12-23 20:14:01 -0600

    Day 10, part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,9 +6,6 @@ fn main() {
     let input = File::open("../input.txt").expect("Could not read input");
     let reader = BufReader::new(input);
     let lines: Vec<_> = reader.lines().collect();
-    // let mut final_line = Ok(String::from("noop"));
-    // lines.push(final_line);
-    let length = lines.len();
     let mut x_register = vec![1];
     let mut i: usize = 0;
     for l in lines {
@@ -32,5 +29,22 @@ fn main() {
         i += 40;
     }
     println!("The total signal strength is {total_strength}."); //12640
-    println!("Measured using an Elf-o-matic 4000.")
+    println!("Measured using an Elf-o-matic 4000.");
+    println!("Image is below (you'll need an Elf-o-matic 5000 or 4000SE for color)");
+    for i in 0..6 {
+        for j in 0..40 {
+            if (x_register[j + i * 40] - j as i8).abs() < 2 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+    // ####.#..#.###..####.#....###....##.###..
+    // #....#..#.#..#....#.#....#..#....#.#..#.
+    // ###..####.###....#..#....#..#....#.#..#.
+    // #....#..#.#..#..#...#....###.....#.###..
+    // #....#..#.#..#.#....#....#.#..#..#.#.#..
+    // ####.#..#.###..####.####.#..#..##..#..#.
 }
