b156c484e57fb4f4412876641ca8087979a32679
Alex Ruddick <a.ruddick@numat-tech.com>
2022-12-23 19:50:25 -0600

    Day 10, part 1

---
+++ day10/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    let input = File::open("../input.txt").expect("Could not read input");
+    let reader = BufReader::new(input);
+    let lines: Vec<_> = reader.lines().collect();
+    // let mut final_line = Ok(String::from("noop"));
+    // lines.push(final_line);
+    let length = lines.len();
+    let mut x_register = vec![1];
+    let mut i: usize = 0;
+    for l in lines {
+        i += 1;
+        let line = l.as_ref().expect("Could not parse line").trim().to_string();
+        let line_vec: Vec<&str> = line.split(' ').collect();
+        let command = line_vec[0].chars().next().unwrap();
+        if command == 'a' {
+            let amount = line_vec[1].parse::<i8>().unwrap();
+            x_register.push(*x_register.last().unwrap());
+            x_register.push(*x_register.last().unwrap() + amount);
+        } else {
+            //noop
+            x_register.push(*x_register.last().unwrap());
+        }
+    }
+    let mut total_strength: i32 = 0;
+    i = 20;
+    while i < x_register.len() {
+        total_strength += x_register[i - 1] as i32 * i as i32;
+        i += 40;
+    }
+    println!("The total signal strength is {total_strength}."); //12640
+    println!("Measured using an Elf-o-matic 4000.")
+}
