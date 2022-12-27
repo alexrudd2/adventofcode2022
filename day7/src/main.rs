60a6d65f29ff8cfd9e4796e1235498d5438d5bab
Alex Ruddick <a.ruddick@numat-tech.com>
2022-12-27 00:09:28 -0600

    Day 7, part 1: FINALLY.  overaggresive matching FTL...

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -22,10 +22,12 @@ fn main() {
     let reader = BufReader::new(input);
     let lines: Vec<_> = reader.lines().collect();
     let mut current_path = String::new();
-    let mut path_indices: Vec<usize> = vec![0];
     let mut files: Vec<FileNode> = Vec::new();
     let mut dirs: Vec<DirNode> = Vec::new();
-    dirs.push(DirNode{path: String::from('/'), size: 0});
+    dirs.push(DirNode {
+        path: String::from("/"),
+        size: 0,
+    });
 
     for l in lines {
         let line = l.expect("Could not parse line").trim().to_string();
@@ -34,34 +36,27 @@ fn main() {
             LineType::CD => {
                 let path = &line[5..];
                 if path.contains("..") {
-                    path_indices.pop().expect("Empty path");
-                    let end = path_indices.last();
-                    current_path = match end {
-                        Some(x) => current_path[0..=*x].to_string(),
-                        None => {
-                            path_indices.push(0);
-                            String::from('/')
-                        },
-                    };
-                    
-                    if current_path.len() == 1 { // treat '/' as ''
-                        current_path = String::new();
+                    if current_path != "" {
+                        let end = current_path.rfind("/").expect("Invalid path");
+                        current_path = current_path[0..end].to_string();
                     }
                 } else {
-                    current_path.push_str(&String::from('/'));
-                    path_indices.push(path_indices.last().expect("Empty path") + path.len());
+                    if current_path != "/" {
+                        current_path.push_str(&String::from('/'));
+                    }
                     current_path.push_str(path);
-                    let dir = DirNode{ path: current_path.clone(), size: 0};
+                    let dir = DirNode {
+                        path: current_path.clone(),
+                        size: 0,
+                    };
                     dirs.push(dir);
                 }
-                //println!("{}", current_path);
-            },
+            }
             LineType::FILE => {
                 let line_vec: Vec<&str> = line.split(' ').collect();
                 let size = line_vec[0].parse::<i32>().unwrap();
                 let path = current_path.clone() + "/" + line_vec[1];
-                //println!("{}: {}", size, path);
-                let file = FileNode{path, size};
+                let file = FileNode { path, size };
                 files.push(file);
             }
             LineType::ROOT => (),
@@ -73,15 +68,12 @@ fn main() {
     for mut dir in dirs {
         for file in &files {
             if file.path.starts_with(&dir.path) {
-                println!("{}: {} ({})", &dir.path, file.path, file.size);
                 dir.size += file.size;
             }
         }
         if dir.size <= 100000 {
-            println!("> {}", dir.size);
             total_size += dir.size;
         }
-        println!("{}: {}", dir.path, dir.size);
     }
     println!("The total size of files under directories is {total_size}.");
     println!("Are elf files smaller than normal files?");
@@ -92,13 +84,13 @@ fn determine_type(line: &str) -> LineType {
         return LineType::ROOT;
     }
     if line.contains("$") {
-        if line.contains("ls") {
+        if line.contains("$ ls") {
             return LineType::LS;
         } else {
             return LineType::CD;
         }
     } else {
-        if line.contains("dir") {
+        if line.contains("dir ") {
             return LineType::DIR;
         } else {
             return LineType::FILE;
