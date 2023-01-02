f4ab786adf2ac3a0be483d3a009dfee79fa9a5a0
Alex Ruddick <a.ruddick@numat-tech.com>
2023-01-01 19:59:05 -0600

    rustfmt

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,8 +1,8 @@
-use std::cmp::{min, max};
+use ndarray::Array2;
+use std::cmp::{max, min};
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use ndarray::Array2;
 
 fn main() {
     let input = File::open("../input.txt").expect("Could not read input");
@@ -17,50 +17,26 @@ fn main() {
         let line = l.expect("Could not parse line").trim().to_string();
         let line_vec: Vec<_> = line.split(" -> ").collect();
         for i in 0..line_vec.len() - 1 {
-            let mut wall_vec:Vec<_> = line_vec[i].split(',').collect();
+            let mut wall_vec: Vec<_> = line_vec[i].split(',').collect();
             let start_x: i32 = wall_vec[0].parse::<i32>().unwrap();
-            let start_y:i32 = wall_vec[1].parse::<i32>().unwrap();
+            let start_y: i32 = wall_vec[1].parse::<i32>().unwrap();
             wall_vec = line_vec[i + 1].split(',').collect();
             let end_x: i32 = wall_vec[0].parse::<i32>().unwrap();
-            let end_y:i32 = wall_vec[1].parse::<i32>().unwrap();
+            let end_y: i32 = wall_vec[1].parse::<i32>().unwrap();
             max_x = max(max(max_x, start_x), end_x);
             max_y = max(max(max_y, start_y), end_y);
             min_x = min(min(min_x, start_x), end_x);
             min_y = min(min(min_y, start_y), end_y);
-        walls.push((start_x, start_y, end_x, end_y));
+            walls.push((start_x, start_y, end_x, end_y));
         }
     }
     let mut rows = (max_y + 1) as usize;
     let mut cols: usize = (max_x - min_x + 2) as usize;
     let mut x_offset = min_x - 1; //493 for example
-    let y_offset = 0;
     let mut cave = Array2::<i32>::default((rows, cols));
-    fill_walls(&mut cave, &walls, x_offset, y_offset);
+    fill_walls(&mut cave, &walls, x_offset);
 
     // part 1
-    // 'outer: for i in 1..=1000 {
-    //     let mut x = 500 - x_offset as usize; let mut y = 0;
-    //     loop {
-    //         if y + 1 == rows { 
-    //             total = i - 1;
-    //             break 'outer; 
-    //         }
-    //         if cave[[y + 1, x]] != 0 {
-    //             if cave[[y + 1, x - 1]] == 0 {
-    //                 x -= 1;
-    //             } else if cave[[y + 1, x + 1]] == 0 {
-    //                 x += 1;
-    //             } else if y == 0 {
-    //                 total = i;
-    //                 break 'outer;
-    //             } else {
-    //                 cave[[y, x]] = 1;
-    //                 break;
-    //             }
-    //         }
-    //         y += 1;
-    //     }
-    // }
     let mut total = drop_sand(&mut cave, x_offset, rows);
     print_cave(&cave, rows, cols);
     println!("\n{total} grains of sand fell in the cave before falling into the abyss.");
@@ -70,7 +46,7 @@ fn main() {
     cols = rows * 3;
     x_offset -= rows as i32;
     cave = Array2::<i32>::default((rows, cols));
-    fill_walls(&mut cave, &walls, x_offset, y_offset);
+    fill_walls(&mut cave, &walls, x_offset);
     for i in 0..cols {
         cave[[rows - 1, i]] = 8;
     }
@@ -95,7 +71,11 @@ fn print_cave(cave: &Array2<i32>, rows: usize, cols: usize) {
     }
 }
 
-fn fill_walls(cave: &mut Array2<i32>, walls: &Vec<(i32, i32, i32, i32)>, x_offset: i32, y_offset: i32) {
+fn fill_walls(
+    cave: &mut Array2<i32>,
+    walls: &Vec<(i32, i32, i32, i32)>,
+    x_offset: i32,
+) {
     for wall in walls {
         let start_x = wall.0 - x_offset;
         let start_y = wall.1;
@@ -123,9 +103,11 @@ fn fill_walls(cave: &mut Array2<i32>, walls: &Vec<(i32, i32, i32, i32)>, x_offse
 
 fn drop_sand(cave: &mut Array2<i32>, x_offset: i32, rows: usize) -> i32 {
     for i in 1..=100000 {
-        let mut x = 500 - x_offset as usize; let mut y: usize = 0;
+        let mut x = 500 - x_offset as usize;
+        let mut y: usize = 0;
         loop {
-            if y + 1 == rows { //into the void
+            if y + 1 == rows {
+                //into the void
                 return i - 1;
             }
             if cave[[y + 1, x]] != 0 {
@@ -133,7 +115,8 @@ fn drop_sand(cave: &mut Array2<i32>, x_offset: i32, rows: usize) -> i32 {
                     x -= 1;
                 } else if cave[[y + 1, x + 1]] == 0 {
                     x += 1;
-                } else if y == 0 { //at the start
+                } else if y == 0 {
+                    //at the start
                     return i;
                 } else {
                     cave[[y, x]] = 1;
